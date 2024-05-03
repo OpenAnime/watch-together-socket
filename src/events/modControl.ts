4fcaf576d43d227309557c910c4215f90fe4c72c
SpongeBed81 <cmodz7969@gmail.com>
2024-05-03 19:39:48 +0300

    feat: ability to change video control permission

---
+++ src/events/modControl.ts
@@ -0,0 +1,33 @@
+import type { Server, Socket } from 'socket.io';
+
+import { get, set } from '@utils/cache';
+
+import { Participant } from './login';
+
+export default class ChangeModeratorControl {
+    async handle({ socket, io, data }: { socket: Socket; io: Server; data: any }) {
+        const controlledByMods = data?.controlledByMods;
+
+        if (!(controlledByMods == true || controlledByMods == false)) return;
+
+        const rooms = Array.from(socket.rooms);
+
+        const room = rooms[1];
+
+        if (!room) return;
+
+        const socketId = socket.id;
+
+        const socketRoomParticipants = (await get(`room:${room}:users`)) as Participant[];
+
+        const user = socketRoomParticipants.find((user) => user.sid == socketId);
+
+        if (user.owner) {
+            await set(`room:${room}:controlledByMods`, controlledByMods);
+
+            io.in(room).emit('modControl', {
+                controlledByMods,
+            });
+        }
+    }
+}
--- src/utils/canDoModerationOperationOnTarget.ts
+++ src/utils/canDoModerationOperationOnTarget.ts
@@ -3,6 +3,7 @@ export default function canDoModerationOperationOnTarget(
     requestingParticipant: Participant,
     targetParticipant: Participant,
 ) {
+    if (!requestingParticipant || !targetParticipant) return false;
     if (!requestingParticipant.moderator) return false;
     if (requestingParticipant.id == targetParticipant.id) return false;
     if (requestingParticipant.owner) return true;
